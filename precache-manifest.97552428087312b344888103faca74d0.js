18a4fe357988a0af273d391ef2d1459b32e56d7d
aakashdhoundiyal_N0275 <aakash.dhoundiyal@jabong.com>
2019-07-15 14:13:24 +0530

    Updates

---
+++ precache-manifest.97552428087312b344888103faca74d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9829953584d91b56f5029fee1f7534",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2f9d9e31ddbc39859965",
+    "url": "/monster-rolodex/static/css/main.d0514284.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2f9d9e31ddbc39859965",
+    "url": "/monster-rolodex/static/js/main.864f5911.chunk.js"
+  },
+  {
+    "revision": "344166cfeaf30c2159bd",
+    "url": "/monster-rolodex/static/js/runtime~main.5df944a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
